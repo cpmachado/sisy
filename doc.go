349ce06cc109da2eb4c2989ae204c0cb4702f8e1
cpmachado <cpmachado@protonmail.com>
2026-07-18 00:41:04 +0100

    doc.go

---
+++ doc.go
@@ -0,0 +1,12 @@
+/*
+Package sisy implements the core definitions of a Task Driven Depression (TDD)
+system.
+
+The primary goal is to make it a simpler, lighter and joke driven task
+environment. Basically, make the burden lighter.
+
+The secondary goal is to be as simple as possible, but extensible and easy to
+integrate with larger oppressing systems, such as GitHub Issues, GitLab Issues,
+or Jira.
+*/
+package sisy
