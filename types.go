4f94b75a098d4a10a6f2dc51f4fc916b6ad1177a
cpmachado <cpmachado@protonmail.com>
2026-07-18 00:12:52 +0100

    some godoc

--- types.go
+++ types.go
@@ -2,6 +2,7 @@ package sisy
 
 import "time"
 
+// a definition of a task
 type Task struct {
 	Title        string     `json:"title"`
 	Description  string     `json:"description"`
@@ -12,16 +13,20 @@ type Task struct {
 	Tags         []Tag      `json:"tags"`
 }
 
+// A definition of a recurring task
 type RecurringTask struct {
 	Current *Task `json:"current"`
 	Next    func(Task) Task
 }
 
+// A definition of a time block
 type TimeBlock struct {
 	Start *time.Time `json:"start"`
 	End   *time.Time `json:"end"`
 }
 
+// a definition of a category
 type Category string
 
+// a definition of a tag
 type Tag string
