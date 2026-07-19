2a381c42388eadba642c0c68c4b672d335711f73
cpmachado <cpmachado@protonmail.com>
2026-07-19 23:10:27 +0100

    Stringer for description

--- todotxt/description.go
+++ todotxt/description.go
@@ -1,9 +1,34 @@
 package todotxt
 
+import "strings"
+
 // description definition
 type Description struct {
 	Text           string
 	ProjectTags    []string
 	ProjectContext []string
 	CustomTags     []CustomTag
+}
+
+func (d *Description) String() string {
+	sb := &strings.Builder{}
+
+	sb.WriteString(d.Text)
+
+	for _, tag := range d.ProjectTags {
+		sb.WriteString(" +")
+		sb.WriteString(tag)
+	}
+
+	for _, ctx := range d.ProjectContext {
+		sb.WriteString(" @")
+		sb.WriteString(ctx)
+	}
+
+	for _, tag := range d.CustomTags {
+		sb.WriteString(" ")
+		sb.WriteString(tag.String())
+	}
+
+	return d.Text
 }
