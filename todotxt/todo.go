6c5fead60e732eaf6b2aca34816fd49c0e1fb729
cpmachado <cpmachado@protonmail.com>
2026-07-19 23:11:40 +0100

    stringer for todo

--- todotxt/todo.go
+++ todotxt/todo.go
@@ -1,12 +1,41 @@
 package todotxt
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 // Todo definition
 type Todo struct {
-	Complete       *bool
+	Complete       bool
 	Priority       *Priority
 	CompletionDate *time.Time
 	CreationDate   *time.Time
 	Description    Description
+}
+
+func (t *Todo) String() string {
+	sb := &strings.Builder{}
+	if t.Complete {
+		sb.WriteString("x ")
+	}
+
+	if t.Priority != nil {
+		sb.WriteString(t.Priority.String())
+		sb.WriteString(" ")
+	}
+
+	if t.CompletionDate != nil {
+		sb.WriteString(t.CompletionDate.Format(time.DateTime))
+		sb.WriteString(" ")
+	}
+
+	if t.CreationDate != nil {
+		sb.WriteString(t.CompletionDate.Format(time.DateTime))
+		sb.WriteString(" ")
+	}
+
+	sb.WriteString(t.Description.String())
+
+	return sb.String()
 }
