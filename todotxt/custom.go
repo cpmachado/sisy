14340fe34353cd32a11c52c5534ff238c20e8fda
cpmachado <cpmachado@protonmail.com>
2026-07-19 23:09:53 +0100

    stringer for CustomTag

--- todotxt/custom.go
+++ todotxt/custom.go
@@ -1,7 +1,13 @@
 package todotxt
 
+import "fmt"
+
 // extension parser definition
 type CustomTag struct {
 	Key   string
 	Value string
+}
+
+func (c *CustomTag) String() string {
+	return fmt.Sprintf("%s:%s", c.Key, c.Value)
 }
