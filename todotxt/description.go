d5e0e7ad2cd68bcb8438e6f16bc740b2c0823788
cpmachado <cpmachado@protonmail.com>
2026-07-20 00:13:59 +0100

    Remove custom and green test for projectTags

--- todotxt/description.go
+++ todotxt/description.go
@@ -1,13 +1,34 @@
 package todotxt
 
-// description definition
+import "regexp"
+
+// description type, parses tags and context and customtags lazily
 type Description struct {
-	Text           string
-	ProjectTags    []string
-	ProjectContext []string
-	CustomTags     []CustomTag
+	Text    string
+	parsed  bool
+	project []string
+	context []string
+	special map[string]any
 }
 
 func (d *Description) String() string {
 	return d.Text
+}
+
+func (d *Description) ProjectTags() []string {
+	if !d.parsed {
+		parseDescription(d)
+	}
+
+	return d.project
+}
+
+func parseDescription(d *Description) {
+	projectRE := regexp.MustCompile(`\+[^\s]+`)
+
+	for _, tag := range projectRE.FindAllString(d.Text, -1) {
+		d.project = append(d.project, tag[1:])
+	}
+
+	d.parsed = true
 }
