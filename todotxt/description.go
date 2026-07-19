bcf7ce23e85edbd84fa44b4f5ab218d73f06c33b
cpmachado <cpmachado@protonmail.com>
2026-07-20 00:21:15 +0100

    Green test ContextTags

--- todotxt/description.go
+++ todotxt/description.go
@@ -23,11 +23,27 @@ func (d *Description) ProjectTags() []string {
 	return d.project
 }
 
+func (d *Description) ContextTags() []string {
+	if !d.parsed {
+		parseDescription(d)
+	}
+
+	return d.context
+}
+
 func parseDescription(d *Description) {
+	if d.parsed {
+		return
+	}
 	projectRE := regexp.MustCompile(`\+[^\s]+`)
+	contextRE := regexp.MustCompile(`@[^\s]+`)
 
 	for _, tag := range projectRE.FindAllString(d.Text, -1) {
 		d.project = append(d.project, tag[1:])
+	}
+
+	for _, ctx := range contextRE.FindAllString(d.Text, -1) {
+		d.context = append(d.context, ctx[1:])
 	}
 
 	d.parsed = true
