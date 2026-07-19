564db0d397b415adf4a8806810cd2c8cb9ee526f
cpmachado <cpmachado@protonmail.com>
2026-07-20 00:13:27 +0100

    Test ProjectTags method

--- todotxt/description_test.go
+++ todotxt/description_test.go
@@ -1,6 +1,7 @@
 package todotxt
 
 import (
+	"slices"
 	"testing"
 )
 
@@ -50,6 +51,58 @@ func TestDescription_String(t *testing.T) {
 
 			if got != want {
 				t.Errorf("String() = %v, want %v", got, want)
+			}
+		})
+	}
+}
+
+func TestDescription_ProjectTags(t *testing.T) {
+	tests := []struct {
+		name        string // description of this test case
+		want        []string
+		description Description
+	}{
+		{
+			name: "one context",
+			want: nil,
+			description: Description{
+				Text: "Thank Mom for the meatballs @phone",
+			},
+		},
+		{
+			name: "one tag one context",
+			want: []string{"GarageSale"},
+			description: Description{
+				Text: "Schedule Goodwill pickup +GarageSale @phone",
+			},
+		},
+		{
+			name: "one tag",
+			want: []string{"GarageSale"},
+			description: Description{
+				Text: "Post signs around the neighborhood +GarageSale",
+			},
+		},
+		{
+			name: "context interpolated with text",
+			want: nil,
+			description: Description{
+				Text: "@GroceryStore pies",
+			},
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			description := tt.description
+			want := tt.want
+
+			// when
+			got := description.ProjectTags()
+
+			if !slices.Equal(got, want) {
+				t.Errorf("ProjectTags() = %v, want %v", got, tt.want)
 			}
 		})
 	}
