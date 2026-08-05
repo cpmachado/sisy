afb3e36799318e0ae62e793126b5f91bd5bbeeff
cpmachado <cpmachado@protonmail.com>
2026-08-05 15:36:13 +0100

    fix doc

--- todotxt/doc.go
+++ todotxt/doc.go
@@ -1,5 +1,3 @@
-/*
-Package todotxt implements a package to handle todotxt compliant [Todo] parsing,
-formatting and others.
-*/
+// Package todotxt implements a package to handle todotxt compliant [Todo] parsing,
+// formatting and others.
 package todotxt
