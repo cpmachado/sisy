1a3cdd7af44ac693b12f9fe589b1bfa9529172cf
cpmachado <cpmachado@protonmail.com>
2026-08-05 14:09:28 +0100

    to string test

---
+++ todotxt/priority_test.go
@@ -0,0 +1,30 @@
+package todotxt
+
+import (
+	"testing"
+)
+
+func TestPriority_String(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		want string
+		p    Priority
+	}{
+		{name: "a", want: "(a)", p: Priority('a')},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			p := tt.p
+			want := tt.want
+
+			// when
+			got := p.String()
+
+			// then
+			if got != want {
+				t.Errorf("String() = %v, want %v", got, want)
+			}
+		})
+	}
+}
